c62d46ffcde0437aaac27e1ec5be4440496bbb12
Jeffery <jeffery@boost.co.nz>
2016-07-25 12:49:28 +1200

    editor link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'contenteditor#index'
   get '/editor' => 'contenteditor#index'
   get '/editor/:id' => 'contenteditor#show'
 end
